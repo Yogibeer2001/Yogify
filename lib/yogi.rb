dd92066beecb9abf4e8fd9f3db9198ed50c9af56
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-07-21 18:32:05 +1000

    working on scoping issue5

--- lib/yogi.rb
+++ lib/yogi.rb
@@ -52,24 +52,16 @@ module Yogi
         text =  File.open(file_name, "r"){ |file| file.read }#File.read(file_name)
         # puts "editing #{$file_sample}"
 
-
-        $pre_counted_comma = count_em(text,",")
-        $pre_counted_semicolon = count_em(text,";")
-        $pre_counted_l = count_em(text,"l")
-        $pre_counted_3 = count_em(text,"3")
-        $pre_counted_s = count_em(text,"s")
-        $pre_counted_bracket = count_em(text,"}")
-        $pre_counted_px = count_em(text,"px")
-       File.open('.ignoreme', "w") {|file| file.puts $pre_counted_comma
-                                           file.puts $pre_counted_semicolon
-                                           file.puts $pre_counted_l
-                                           file.puts $pre_counted_3
-                                           file.puts $pre_counted_s
-                                           file.puts $pre_counted_bracket
-                                           file.puts $pre_counted_px
-                                         }
-        counter_test = IO.readlines(".ignoreme")[2]
-       puts "pre_counted_l schould be : #{counter_test}"
+pre_counted_hash {
+        $pre_counted_comma => count_em(text,",")
+        $pre_counted_semicolon => count_em(text,";")
+        $pre_counted_l => count_em(text,"l")
+        $pre_counted_3 => count_em(text,"3")
+        $pre_counted_s => count_em(text,"s")
+        $pre_counted_bracket => count_em(text,"}")
+        $pre_counted_px => count_em(text,"px")
+}
+       File.open('.ignoreme', "w") {|file| file.puts pre_counted_hash}
         # puts "commas : #{$pre_counted_comma}"
         # puts "semicolons : #{$pre_counted_semicolon}"
         # puts "l : #{$pre_counted_l}"
