42146b492c53d9233ba685c2939bd276afe355b2
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-07-21 16:52:29 +1000

    working on scoping issue

--- lib/yogi.rb
+++ lib/yogi.rb
@@ -7,21 +7,21 @@ $file_sample = $file_names.sample(5)
 
 
 module Yogi
-  @@pre_counted_comma = 0
-  @@pre_counted_semicolon = 0
-  @@pre_counted_l = 0
-  @@pre_counted_3 = 0
-  @@pre_counted_s = 0
-  @@pre_counted_bracket = 0
-  @@pre_counted_px = 0
+  $pre_counted_comma = 0
+  $pre_counted_semicolon = 0
+  $pre_counted_l = 0
+  $pre_counted_3 = 0
+  $pre_counted_s = 0
+  $pre_counted_bracket = 0
+  $pre_counted_px = 0
 
-  @@pre_diff_comma = 0
-  @@pre_diff_semicolon = 0
-  @@pre_diff_l = 0
-  @@pre_diff_3 = 0
-  @@pre_diff_s = 0
-  @@pre_diff_bracket = 0
-  @@pre_diff_px = 0
+  $pre_diff_comma = 0
+  $pre_diff_semicolon = 0
+  $pre_diff_l = 0
+  $pre_diff_3 = 0
+  $pre_diff_s = 0
+  $pre_diff_bracket = 0
+  $pre_diff_px = 0
 
   class ErrorInside
 
@@ -52,13 +52,13 @@ module Yogi
         text =  File.open(file_name, "r"){ |file| file.read }#File.read(file_name)
         # puts "editing #{$file_sample}"
 
-        @@pre_counted_comma = count_em(text,",")
-        @@pre_counted_semicolon = count_em(text,";")
-        @@pre_counted_l = count_em(text,"l")
-        @@pre_counted_3 = count_em(text,"3")
-        @@pre_counted_s = count_em(text,"s")
-        @@pre_counted_bracket = count_em(text,"}")
-        @@pre_counted_px = count_em(text,"px")
+        self.$pre_counted_comma = count_em(text,",")
+        self.$pre_counted_semicolon = count_em(text,";")
+        self.$pre_counted_l = count_em(text,"l")
+        self.$pre_counted_3 = count_em(text,"3")
+        self.$pre_counted_s = count_em(text,"s")
+        self.$pre_counted_bracket = count_em(text,"}")
+        self.$pre_counted_px = count_em(text,"px")
 
         # puts "commas : #{$pre_counted_comma}"
         # puts "semicolons : #{$pre_counted_semicolon}"
@@ -108,13 +108,13 @@ module Yogi
         # post_total = post_counted_comma + post_counted_semicolon + post_counted_l + post_counted_3 + post_counted_s + post_counted_bracket + post_counted_px
         # puts post_total
 
-        @@pre_diff_comma = @@pre_counted_comma - post_counted_comma
-        @@pre_diff_semicolon = @@pre_counted_semicolon - post_counted_semicolon
-        @@pre_diff_l = @@pre_counted_l - post_counted_l
-        @@pre_diff_3 = @@pre_counted_3 - post_counted_3
-        @@pre_diff_s = @@pre_counted_s - post_counted_s
-        @@pre_diff_bracket = @@pre_counted_bracket - post_counted_bracket
-        @@pre_diff_px = @@pre_counted_px - post_counted_px
+        self.$pre_diff_comma = self.$pre_counted_comma - post_counted_comma
+        self.$pre_diff_semicolon = self.$pre_counted_semicolon - post_counted_semicolon
+        self.$pre_diff_l = self.$pre_counted_l - post_counted_l
+        self.$pre_diff_3 = self.$pre_counted_3 - post_counted_3
+        self.$pre_diff_s = self.$pre_counted_s - post_counted_s
+        self.$pre_diff_bracket = self.$pre_counted_bracket - post_counted_bracket
+        self.$pre_diff_px = self.$pre_counted_px - post_counted_px
 
         # puts "commas : #{$pre_diff_comma}"
         # puts "semicolons : #{$pre_diff_semicolon}"
@@ -134,15 +134,15 @@ module Yogi
       text.scan(/(?=#{substring})/).count
     end
 
-    def checker
+      def checker
 
-    puts  @@pre_diff_comma
-    puts  @@pre_diff_semicolon
-    puts  @@pre_diff_l
-    puts  @@pre_diff_3
-    puts  @@pre_diff_s
-    puts  @@pre_diff_bracket
-    puts  @@pre_diff_px
+    puts  self.$pre_diff_comma
+    puts  self.$pre_diff_semicolon
+    puts  self.$pre_diff_l
+    puts  self.$pre_diff_3
+    puts  self.$pre_diff_s
+    puts  self.$pre_diff_bracket
+    puts  self.$pre_diff_px
 
 
       $file_sample.each do |file_name|
@@ -162,67 +162,67 @@ module Yogi
         # puts "s : #{post_counted_s}"
         # puts "} : #{post_counted_bracket}"
         # puts "px : #{post_counted_px}"
-puts (@@pre_counted_comma)
-puts (@@pre_counted_semicolon)
-puts (@@pre_counted_l)
-puts (@@pre_counted_3)
-puts (@@pre_counted_s)
-puts (@@pre_counted_bracket)
-puts (@@pre_counted_px)
-        post_diff_comma = @@pre_counted_comma - post_counted_comma
-        post_diff_semicolon = @@pre_counted_semicolon - post_counted_semicolon
-        post_diff_l = @@pre_counted_l - post_counted_l
-        post_diff_3 = @@pre_counted_3 - post_counted_3
-        post_diff_s = @@pre_counted_s - post_counted_s
-        post_diff_bracket = @@pre_counted_bracket - post_counted_bracket
-        post_diff_px = @@pre_counted_px - post_counted_px
+puts (self.$pre_counted_comma)
+puts (self.$pre_counted_semicolon)
+puts (self.$pre_counted_l)
+puts (self.$pre_counted_3)
+puts (self.$pre_counted_s)
+puts (self.$pre_counted_bracket)
+puts (self.$pre_counted_px)
+        post_diff_comma = self.$pre_counted_comma - post_counted_comma
+        post_diff_semicolon = self.$pre_counted_semicolon - post_counted_semicolon
+        post_diff_l = self.$pre_counted_l - post_counted_l
+        post_diff_3 = self.$pre_counted_3 - post_counted_3
+        post_diff_s = self.$pre_counted_s - post_counted_s
+        post_diff_bracket = self.$pre_counted_bracket - post_counted_bracket
+        post_diff_px = self.$pre_counted_px - post_counted_px
 
-        if @@pre_diff_comma == 0
+        if self.$pre_diff_comma == 0
           comma_fix = 100
         else
-        comma_fix = ((@@pre_diff_comma - post_diff_comma)/@@pre_diff_comma)*100
+        comma_fix = ((self.$pre_diff_comma - post_diff_comma)/self.$pre_diff_comma)*100
         end
         puts " #{comma_fix}% of comma errors fixed"
 
-        if @@pre_diff_semicolon == 0
+        if self.$pre_diff_semicolon == 0
            semicolon_fix = 100
         else
-        semicolon_fix = ((@@pre_diff_semicolon - post_diff_semicolon)/@@pre_diff_semicolon)*100
+        semicolon_fix = ((self.$pre_diff_semicolon - post_diff_semicolon)/self.$pre_diff_semicolon)*100
         end
         puts " #{semicolon_fix}% of comma errors fixed"
 
-        if @@pre_diff_l == 0
+        if self.$pre_diff_l == 0
            l_fix = 100
         else
-        l_fix = ((@@pre_diff_l - post_diff_l)/@@pre_diff_l)*100
+        l_fix = ((self.$pre_diff_l - post_diff_l)/self.$pre_diff_l)*100
         end
         puts " #{l_fix}% of comma errors fixed"
 
-        if @@pre_diff_3 == 0
+        if self.$pre_diff_3 == 0
            three_fix = 100
         else
-        three_fix = ((@@pre_diff_3 - post_diff_3)/@@pre_diff_3)*100
+        three_fix = ((self.$pre_diff_3 - post_diff_3)/self.$pre_diff_3)*100
         end
         puts " #{three_fix}% of comma errors fixed"
 
-        if @@pre_diff_s == 0
+        if self.$pre_diff_s == 0
            s_fix = 100
         else
-        s_fix = ((@@pre_diff_s - post_diff_s)/@@pre_diff_s)*100
+        s_fix = ((self.$pre_diff_s - post_diff_s)/self.$pre_diff_s)*100
         end
         puts " #{s_fix}% of comma errors fixed"
 
-        if @@pre_diff_bracket == 0
+        if self.$pre_diff_bracket == 0
            bracket_fix = 100
         else
-        bracket_fix = ((@@pre_diff_bracket - post_diff_bracket)/@@pre_diff_bracket)*100
+        bracket_fix = ((self.$pre_diff_bracket - post_diff_bracket)/self.$pre_diff_bracket)*100
         end
         puts " #{bracket_fix}% of comma errors fixed"
 
-        if @@pre_diff_px == 0
+        if self.$pre_diff_px == 0
            px_fix = 100
         else
-        px_fix = ((@@pre_diff_px - post_diff_px)/@@pre_diff_px)*100
+        px_fix = ((self.$pre_diff_px - post_diff_px)/self.$pre_diff_px)*100
         end
         puts " #{px_fix}% of comma errors fixed"
       end
