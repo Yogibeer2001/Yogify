4290d1f486608eb310a27d6770cbf1d6a05b6790
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-07-29 13:10:47 +1000

    cleaning up code

--- lib/yogi.rb
+++ lib/yogi.rb
@@ -7,16 +7,6 @@ require 'shellwords'
 
 module Yogi
 
-  class Setup
-    def setup
-      $file_names = []
-      $file_names = Dir.glob("app/**/*.rb") + Dir.glob("app/**/*.js") + Dir.glob("app/**/*.css") + Dir.glob("app/**/*.scss") + Dir.glob("app/**/*.erb") + Dir.glob("app/**/*.html") + Dir.glob("config/routes.rb")
-      $sample_size = 7
-      $file_sample = $file_names.sample($sample_size)
-      File.open('.git/.ignoremefile.txt', "a") {|file| file.puts $file_sample.to_json}
-    end
-  end
-
   $pre_counted_comma = 0
   $pre_counted_semicolon = 0
   $pre_counted_l = 0
@@ -37,6 +27,17 @@ module Yogi
   $pre_diff_sq_bracket = 0
   $pre_diff_equal = 0
 
+
+  class Setup
+    def setup
+      $file_names = []
+      $file_names = Dir.glob("app/**/*.rb") + Dir.glob("app/**/*.js") + Dir.glob("app/**/*.css") + Dir.glob("app/**/*.scss") + Dir.glob("app/**/*.erb") + Dir.glob("app/**/*.html") + Dir.glob("config/routes.rb")
+      $sample_size = 7
+      $file_sample = $file_names.sample($sample_size)
+      File.open('.git/.ignoremefile.txt', "a") {|file| file.puts $file_sample.to_json}
+    end
+  end
+
   class ErrorInside
 
     def count_em(text, substring)
@@ -46,32 +47,20 @@ module Yogi
     # creating backup directory
     def backup
         FileUtils.mkdir_p '.git/.backupFiles' unless File.exists?('.git/.backupFiles')
-        # puts "created folder backupFiles"
 
         #copy backup to backup folder
         FileUtils.cp_r "./app", ".git/.backupFiles/"
         FileUtils.cp_r "./config", ".git/.backupFiles/"
-        # puts "copied files to backupFiles #{$file_names}"
-
-        # #rename files in backupFiles folder
-        #   backup_file_path = "./backupFiles/"
-        #   Dir.foreach('./backupFiles') do |item|
-        #     next if item == '.' or item == '..'
-        #     File.rename(backup_file_path+item, backup_file_path+item +".bak")
-        #   end
-        # end
     end
 
     def yogify
       count_hash = []
       buffer = File.open('.git/.ignoremefile.txt', 'r').read
       file_sample = JSON.parse(buffer)
-      # puts file_sample.class
-      # puts file_sample
 
       file_sample.each do |file_name|
         text =  File.open(file_name, "r"){ |file| file.read }#File.read(file_name)
-        # puts "editing #{$file_sample}"
+
         $pre_counted_comma = count_em(text,",")
         $pre_counted_semicolon = count_em(text,";")
         $pre_counted_l = count_em(text,"l")
@@ -81,18 +70,6 @@ module Yogi
         $pre_counted_px = count_em(text,"px")
         $pre_counted_sq_bracket = count_em(text,">")
         $pre_counted_equal = count_em(text,"==")
-
-        # puts "commas : #{$pre_counted_comma}"
-        # puts "semicolons : #{$pre_counted_semicolon}"
-        # puts "l : #{$pre_counted_l}"
-        # puts "3 : #{$pre_counted_3}"
-        # puts "s : #{$pre_counted_s}"
-        # puts "} : #{$pre_counted_bracket}"
-        # puts "px : #{$pre_counted_px}"
-
-        #counts total symbols to be effected
-        # pre_total = pre_counted_comma + pre_counted_semicolon + pre_counted_l + pre_counted_3 + pre_counted_s + pre_counted_bracket + pre_counted_px
-        # puts pre_total
 
         # To merely print the contents of the file, use:
         new_contents1 = text.gsub(";"){rand(2).zero? ? ";" : ":"}
@@ -105,13 +82,10 @@ module Yogi
         new_contents8 = new_contents7.gsub(">"){rand(2).zero? ? "<" : "."}
         new_contents9 = new_contents8.gsub("=="){rand(2).zero? ? "==" : "="}
 
-        # puts new_contents6
-
         # To write changes to the file, use:
         File.open(file_name, "w") {|file| file.puts new_contents9 }
 
         text =  File.open(file_name, "r"){ |file| file.read }#File.read(file_name)
-        # puts text
         #counts ocurences in the file after initial change
         post_counted_comma = count_em(text,",")
         post_counted_semicolon = count_em(text,";")
@@ -122,17 +96,6 @@ module Yogi
         post_counted_px = count_em(text,"px")
         post_counted_sq_bracket = count_em(text,">")
         post_counted_equal = count_em(text,"==")
-
-        # puts "commas : #{post_counted_comma}"
-        # puts "semicolons : #{post_counted_semicolon}"
-        # puts "l : #{post_counted_l}"
-        # puts "3 : #{post_counted_3}"
-        # puts "s : #{post_counted_s}"
-        # puts "} : #{post_counted_bracket}"
-        # puts "px : #{post_counted_px}"
-        #counts total symbols to be effected
-        # post_total = post_counted_comma + post_counted_semicolon + post_counted_l + post_counted_3 + post_counted_s + post_counted_bracket + post_counted_px
-        # puts post_total
 
         $pre_diff_comma = $pre_counted_comma - post_counted_comma
         $pre_diff_semicolon = $pre_counted_semicolon - post_counted_semicolon
@@ -165,19 +128,6 @@ module Yogi
           "pre_diff_equal": $pre_diff_equal
         }}
           count_hash << pre_count_hash
-
-        # puts "commas : #{$pre_diff_comma}"
-        # puts "semicolons : #{$pre_diff_semicolon}"
-        # puts "l : #{$pre_diff_l}"
-        # puts "3 : #{$pre_diff_3}"
-        # puts "s : #{$pre_diff_s}"
-        # puts "} : #{$pre_diff_bracket}"
-        # puts "px : #{$pre_diff_px}"
-
-        # json_file = File.read(".ignoreme.json")
-        # variable_hash = JSON.parse(json_file)
-        # # counter_test = variable_hash[file_name]
-        # puts "pre_counted_l schould be : #{variable_hash}"
       end
       File.open('.git/.ignoreme.json', "a") {|file| file.write count_hash.to_json}
       puts "You can start your debugging..."
@@ -187,13 +137,6 @@ module Yogi
         escfile = Shellwords.escape(file)
         cmd = "afplay #{escfile}"
         system cmd
-
-
-      # cmd = ("say 'Debugging mode activated'")
-      # exec cmd
-      # cmd = ("afplay 'sound/activated.wav'")
-      # exec cmd
-
       end
     end
   end
@@ -210,8 +153,6 @@ module Yogi
       post_diff_array = []
       buffer = File.open('.git/.ignoremefile.txt', 'r').read
       file_sample = JSON.parse(buffer)
-      # puts file_sample.class
-      # puts file_sample
 
       file_sample.each do |file_name|
         text =  File.open(file_name, "r"){ |file| file.read }#File.read(file_name)
@@ -268,92 +209,9 @@ module Yogi
 
         pre_diff_array << total_pre_diff
         post_diff_array << total_post_diff
-
-        # puts '--------------------------------------------------------------------'
-        # puts "#{file_name}"
-        # puts " total_pre_diff: #{total_pre_diff}"
-        # puts " total_post_diff: #{total_post_diff}"
-        # puts "post commas : #{post_counted_comma}"
-        # puts "post semicolons : #{post_counted_semicolon}"
-        # puts "post l : #{post_counted_l}"
-        # puts "post 3 : #{post_counted_3}"
-        # puts "post s : #{post_counted_s}"
-        # puts "post } : #{post_counted_bracket}"
-        # puts "post px : #{post_counted_px}"
-        # puts  "pre_diff_comma: #{$pre_diff_comma} vs post_diff_comma: #{post_diff_comma}"
-        # puts  "pre_diff_semicolon: #{$pre_diff_semicolon} vs post_diff_semicolon: #{post_diff_semicolon}"
-        # puts  "pre_diff_l: #{$pre_diff_l} vs post_diff_l: #{post_diff_l}"
-        # puts  "pre_diff_3: #{$pre_diff_3} vs post_diff_3: #{post_diff_3}"
-        # puts  "pre_diff_s: #{$pre_diff_s} vs post_diff_s: #{post_diff_s}"
-        # puts  "pre_diff_bracket: #{$pre_diff_bracket} vs post_diff_bracket: #{post_diff_bracket}"
-        # puts  "pre_diff_px: #{$pre_diff_px} vs post_diff_px: #{post_diff_px}"
-        # puts  "pre_counted_comma: #{$pre_counted_comma} vs post_counted_comma: #{post_counted_comma}"
-        # puts  "pre_counted_semicolon: #{$pre_counted_semicolon} vs post_counted_semicolon: #{post_counted_semicolon}"
-        # puts  "pre_counted_l: #{$pre_counted_l} vs post_counted_l: #{post_counted_l}"
-        # puts  "pre_counted_3: #{$pre_counted_3} vs post_counted_3: #{post_counted_3}"
-        # puts  "pre_counted_s: #{$pre_counted_s} vs post_counted_s: #{post_counted_s}"
-        # puts  "pre_counted_bracket: #{$pre_counted_bracket} vs post_counted_bracket: #{post_counted_bracket}"
-        # puts  "pre_counted_px: #{$pre_counted_px} vs post_counted_px: #{post_counted_px}"
-        # puts '--------------------------------------------------------------------'
-
-      #   if $pre_diff_comma == 0
-      #     comma_fix = 100
-      #   else
-      #   comma_fix = (($pre_diff_comma - post_diff_comma)/$pre_diff_comma)*100
-      #   end
-      #   puts " #{comma_fix}% of comma errors fixed"
-      #
-      #   if $pre_diff_semicolon == 0
-      #      semicolon_fix = 100
-      #   else
-      #   semicolon_fix = (($pre_diff_semicolon - post_diff_semicolon)/$pre_diff_semicolon)*100
-      #   end
-      #   puts " #{semicolon_fix}% of comma errors fixed"
-      #
-      #   if $pre_diff_l == 0
-      #      l_fix = 100
-      #   else
-      #   l_fix = (($pre_diff_l - post_diff_l)/$pre_diff_l)*100
-      #   end
-      #   puts " #{l_fix}% of comma errors fixed"
-      #
-      #   if $pre_diff_3 == 0
-      #      three_fix = 100
-      #   else
-      #   three_fix = (($pre_diff_3 - post_diff_3)/$pre_diff_3)*100
-      #   end
-      #   puts " #{three_fix}% of comma errors fixed"
-      #
-      #   if $pre_diff_s == 0
-      #      s_fix = 100
-      #   else
-      #   s_fix = (($pre_diff_s - post_diff_s)/$pre_diff_s)*100
-      #   end
-      #   puts " #{s_fix}% of comma errors fixed"
-      #
-      #   if $pre_diff_bracket == 0
-      #      bracket_fix = 100
-      #   else
-      #   bracket_fix = (($pre_diff_bracket - post_diff_bracket)/$pre_diff_bracket)*100
-      #   end
-      #   puts " #{bracket_fix}% of comma errors fixed"
-      #
-      #   if $pre_diff_px == 0
-      #      px_fix = 100
-      #   else
-      #   px_fix = (($pre_diff_px - post_diff_px)/$pre_diff_px)*100
-      #   end
-      #   puts " #{px_fix}% of comma errors fixed"
       end
-      # puts " pre_diff_array: #{pre_diff_array}"
-      # puts " post_diff_array: #{post_diff_array}"
-
-
-      #calulate the total differences to original and errors
       pre_diff = pre_diff_array.inject(0, :+)
-      # puts "total of pre_diff #{pre_diff}"
       post_diff = post_diff_array.inject(0, :+)
-      # puts "total of post_diff #{post_diff}"
       pre_diff = pre_diff.to_f
       post_diff = post_diff.to_f
 
